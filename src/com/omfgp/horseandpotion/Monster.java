01791f5a290c8ef85a8d30b7afbdc7ff17ba4ff4
Jackson Powell <sandalsftw@gmail.com>
2012-08-31 00:20:46 -0400

    Monster stuff

--- src/com/omfgp/horseandpotion/Monster.java
+++ src/com/omfgp/horseandpotion/Monster.java
@@ -3,4 +3,17 @@ package com.omfgp.horseandpotion;
 public class Monster 
 {
 	
+	String monsterName, monsterClass;
+	int monsterLevel, monsterHealth;
+	
+	public Monster()
+	{
+		
+	}
+	
+	public Monster(String name)
+	{
+		
+	}
+	
 }

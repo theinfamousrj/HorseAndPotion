21330f57ff4f41dd664b4b14035593233c4a89cc
Jackson Powell <sandalsftw@gmail.com>
2012-08-31 13:23:17 -0400

    Filled out Monster class

--- src/com/omfgp/horseandpotion/Monster.java
+++ src/com/omfgp/horseandpotion/Monster.java
@@ -3,13 +3,54 @@ package com.omfgp.horseandpotion;
 public class Monster 
 {
 	
-	String mName, mClass;
-	int mLevel, mHealth;
+	private String mName, mClass;
+	private int mLevel, mHealth;
 	
 	public Monster() { }
 	
-	public Monster(String name) {
-		this.mName = name;
+	public Monster(String monsterName) 
+	{
+		this.mName = monsterName;
+	}
+	
+	public void setMName(String monsterName)
+	{
+		this.mName = monsterName;
+	}
+	
+	public String getMName()
+	{
+		return this.mName;
+	}
+	
+	public void setMClass(String monsterClass)
+	{
+		this.mClass = monsterClass;
+	}
+	
+	public String getMClass()
+	{
+		return this.mClass;
+	}
+	
+	public void setMLevel(int monsterLevel)
+	{
+		this.mLevel = monsterLevel;
+	}
+	
+	public int getMLevel()
+	{
+		return this.mLevel;
+	}
+	
+	public void setMHealth(int monsterHealth)
+	{
+		this.mHealth = monsterHealth;
+	}
+	
+	public int getMHealth()
+	{
+		return this.mHealth;
 	}
 	
 }
